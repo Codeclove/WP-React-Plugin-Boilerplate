d3e9977edc3f14cd98a4b5bf103af224521fbd6f
trenccan777 <m.chrast@gmail.com>
2022-09-01 12:58:51 +0200

    Update class-test-plugin-post-metas.php

--- includes/admin/class-test-plugin-post-metas.php
+++ includes/admin/class-test-plugin-post-metas.php
@@ -12,55 +12,43 @@ if (!class_exists('\\Test_Plugin\\PostMetas')) {
      */
     class PostMetas
     {
-        private $object_type = 'post';
-        private $meta_args = array( // Validate and sanitize the meta value.
-            // Note: currently (4.7) one of 'string', 'boolean', 'integer',
-            // 'number' must be used as 'type'. The default is 'string'.
-            'type' => 'string',
-            // Custom post type
-            'object_subtype' => '',
-            // Shown in the schema for the meta key.
-            'description' => '',
-            // Return a single value of the type.
-            'single' => true,
-            // Show in the WP REST API response. Default: false.
-            'show_in_rest' => true,
-        );
 
-        /**
-         * __construct
-         *
-         * @param  mixed $post_type
-         * @param  mixed $meta_desc
-         * @return void
-         */
-        public function __construct($post_type, $meta_desc)
+        public $metas = array(
+
+            'test_meta' => array(
+                'type' => 'string',
+                'default' => 'modro-cervena',
+            ),
+            'test_meta_number' => array(
+                'type' => 'number',
+                'default' => 0,
+            ),
+
+            'test_meta_with_schema' => array(
+                'type' => 'array',
+                'show_in_rest' => array(
+                    'schema' => array(
+                        'items' => array(
+                            'type' => 'string',
+
+                        ),
+                    ),
+                ),
+            ),
+       );
+        public function __construct()
         {
+            foreach ($this->metas as $meta_key => $meta_args) {
 
-            $this->meta_args['object_subtype'] = $post_type;
-            $this->meta_args['description'] = $meta_desc;
+                $meta_args['single'] = true;
+                if (!array_key_exists('show_in_rest', $meta_args)) {
+                    $meta_args['show_in_rest'] = true;
+                }
 
-        }
-
-        /**
-         * register_metas
-         *
-         * @param  mixed $args - array of arguments for single meta
-         * @return void
-         */
-        public function register_metas($args)
-        {
-
-            $meta_args = $this->meta_args;
-
-            foreach ($args as $single_meta_args) {
-                $meta_key = $single_meta_args['meta_key'];
-                $meta_args['type'] = $single_meta_args['type'];
-                $meta_args['show_in_rest'] = isset($single_meta_args['show_in_rest']) ? $single_meta_args['show_in_rest'] : true;
-                register_meta($this->object_type, $meta_key, $meta_args);
+                register_meta('post', $meta_key, $meta_args);
             }
 
         }
-    }
 
+    }
 }
