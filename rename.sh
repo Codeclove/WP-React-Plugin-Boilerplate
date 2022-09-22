2ff938b501fa6a06c232628a48e911747492fbba
Marek <m.chrast@gmail.com>
2022-09-22 10:54:32 +0200

    Make rename.sh executable

--- rename.sh
+++ rename.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-find ./ -name "*" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
-
 removed_dash="$(echo $1 | tr '-' ' ' )" 
 original_arr=($removed_dash)
 capitalize="${original_arr[@]^}"
@@ -8,8 +6,10 @@ underscore="$(echo $1 | tr '-' '_' )"
 capitalize_with_underscore="$(echo $capitalize | tr ' ' '_' )" 
 uppercase="${capitalize_with_underscore^^}_"
 
+#Rename keywords in files
 find ./ -type f -exec sed -i -e "s/test-plugin/$1/g" {} \;
 find ./ -type f -exec sed -i -e "s/test_plugin/$underscore/g" {} \;
 find ./ -type f -exec sed -i -e "s/Test_Plugin/$capitalize_with_underscore/g" {} \;
 find ./ -type f -exec sed -i -e "s/TEST_PLUGIN_/$uppercase/g" {} \;
+#Rename files
 find ./ -name "*" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
