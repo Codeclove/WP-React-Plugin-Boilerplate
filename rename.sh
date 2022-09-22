317ce2c2ef755cbad3fc2731cd98d1e0f0cb8a27
Marek <m.chrast@gmail.com>
2022-09-22 11:52:53 +0200

    Update rename.sh file

--- rename.sh
+++ rename.sh
@@ -7,9 +7,10 @@ capitalize_with_underscore="$(echo $capitalize | tr ' ' '_' )"
 uppercase="${capitalize_with_underscore^^}_"
 
 #Rename keywords in files
-find ./ -type f -not -path '*/\.git/*' -exec sed -i -e "s/test-plugin/$1/g" {} \;
-find ./ -type f -not -path '*/\.git/*' -exec sed -i -e "s/test_plugin/$underscore/g" {} \;
-find ./ -type f -not -path '*/\.git/*' -exec sed -i -e "s/Test_Plugin/$capitalize_with_underscore/g" {} \;
-find ./ -type f -not -path '*/\.git/*' -exec sed -i -e "s/TEST_PLUGIN_/$uppercase/g" {} \;
+find ./ -type f -not -path '*/\.git/*' ! -name "rename.sh" -exec sed -i -e "s/test-plugin/$1/g" {} \;
+find ./ -type f -not -path '*/\.git/*' ! -name "rename.sh" -exec sed -i -e "s/test_plugin/$underscore/g" {} \;
+find ./ -type f -not -path '*/\.git/*' ! -name "rename.sh" -exec sed -i -e "s/Test_Plugin/$capitalize_with_underscore/g" {} \;
+find ./ -type f -not -path '*/\.git/*' ! -name "rename.sh" -exec sed -i -e "s/TEST_PLUGIN_/$uppercase/g" {} \;
+
 #Rename files
-find ./ -name "*" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
+find ./ -name "*.php" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
