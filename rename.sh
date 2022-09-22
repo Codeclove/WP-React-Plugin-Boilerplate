752959e7e7b6e0056e86eb607464bcd7878daf1f
Marek <m.chrast@gmail.com>
2022-09-22 10:38:35 +0200

    Add bash script to rename files and keywords

---
+++ rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+find ./ -name "*" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
+
+removed_dash="$(echo $1 | tr '-' ' ' )" 
+original_arr=($removed_dash)
+capitalize="${original_arr[@]^}"
+underscore="$(echo $1 | tr '-' '_' )"
+capitalize_with_underscore="$(echo $capitalize | tr ' ' '_' )" 
+uppercase="${capitalize_with_underscore^^}_"
+
+find ./ -type f -exec sed -i -e "s/test-plugin/$1/g" {} \;
+find ./ -type f -exec sed -i -e "s/test_plugin/$underscore/g" {} \;
+find ./ -type f -exec sed -i -e "s/Test_Plugin/$capitalize_with_underscore/g" {} \;
+find ./ -type f -exec sed -i -e "s/TEST_PLUGIN_/$uppercase/g" {} \;
+find ./ -name "*" -exec rename "s/test-plugin/$1/g" *test-plugin* {} ";" 
